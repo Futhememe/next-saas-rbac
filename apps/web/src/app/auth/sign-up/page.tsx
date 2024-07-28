4395fda13c94b484019d5e8627c19262cc8216eb
Futheme <kyoro_souza@hotmail.com>
2024-07-28 13:26:40 -0300

    ✨ create auth routes

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -7,9 +7,13 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input name="name" id="name" />
+      </div>
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
@@ -17,27 +21,28 @@ export default function SignInPage() {
       <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input name="password" type="password" id="password" />
-
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
       </div>
 
-      <Button type="submit" className="w-full ">
-        Sign in with e-mail
-      </Button>
+      <div className="space-y-1">
+        <Label htmlFor="password-confirmation">Confirm your password</Label>
+        <Input
+          name="password-confirmation"
+          type="password"
+          id="password-confirmation"
+        />
+      </div>
 
+      <Button type="submit" className="w-full">
+        Create account
+      </Button>
       <Button variant={'link'} className="w-full" size="sm" asChild>
-        <Link href="/auth/sign-up">Create a new account</Link>
+        <Link href="/auth/sign-in">Already registered? Sign in</Link>
       </Button>
 
       <Separator />
       <Button type="submit" variant="outline" className="w-full ">
         <GithubLogo size={16} className="mr-2" />
-        Sign in with Github
+        Sign up with Github
       </Button>
     </form>
   )
