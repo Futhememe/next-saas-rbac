741bc16b86c474c1132167553931fe684e8343bc
Futheme <kyoro_souza@hotmail.com>
2024-08-20 21:54:14 -0300

    🎨 new form validations for actions

---
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import { GithubLogo, SpinnerGap, Warning } from '@phosphor-icons/react'
+import Link from 'next/link'
+import { useActionState } from 'react'
+
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+import { signInWithEmailAndPassword } from './actions'
+
+export function SignInForm() {
+  const [{ success, message, errors }, formAction, isPending] = useActionState(
+    signInWithEmailAndPassword,
+    { success: false, message: null, errors: null },
+  )
+
+  return (
+    <form action={formAction} className="space-y-4">
+      {success === false && message && (
+        <Alert variant="destructive">
+          <Warning className="size-4" />
+          <AlertTitle>Sign in failed!</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" type="email" id="email" />
+
+        {errors?.email && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.email[0]}
+          </p>
+        )}
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input name="password" type="password" id="password" />
+        {errors?.password && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.password[0]}
+          </p>
+        )}
+
+        <Link
+          href="/auth/forgot-password"
+          className="text-xs font-medium text-foreground hover:underline"
+        >
+          Forgot your password?
+        </Link>
+      </div>
+
+      <Button type="submit" className="w-full " disabled={isPending}>
+        {isPending ? (
+          <SpinnerGap size={16} className="animate-spin" />
+        ) : (
+          <p>Sign in with Email</p>
+        )}
+      </Button>
+
+      <Button variant={'link'} className="w-full" size="sm" asChild>
+        <Link href="/auth/sign-up">Create a new account</Link>
+      </Button>
+
+      <Separator />
+      <Button type="submit" variant="outline" className="w-full ">
+        <GithubLogo size={16} className="mr-2" />
+        Sign in with Github
+      </Button>
+    </form>
+  )
+}
